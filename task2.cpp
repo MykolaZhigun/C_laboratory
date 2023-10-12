9b948da62dc0a311c4ff0024bc2c6a8d9ac97ef0
Микола Жигун <125123753+MykolaZhigun@users.noreply.github.com>
2023-10-12 21:35:14 +0300

    Create task2.cpp

---
+++ task2.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <string>
+
+class Student {
+public:
+    int age;          
+    std::string gender;  
+    double money; 
+    std::string name;   
+
+    Student(const std::string& studentName, int studentAge, const std::string& studentGender, double studentScholarship) {
+        name = studentName;
+        age = studentAge;
+        gender = studentGender;
+        money = studentScholarship;
+    }
+
+    // Функція для виведення інформації про студента
+    void displayInfo() {
+        std::cout << "Name: " << name << std::endl;
+        std::cout << "Age: " << age << std::endl;
+        std::cout << "Gender: " << gender << std::endl;
+        std::cout << "Scholarship: " << money << std::endl << std::endl;
+    }
+};
+
+int main() {
+    Student student1("Mykola", 18, "Man", 1200.0);
+    Student student2("Oleg", 19, "Man", 1000);
+
+    student1.displayInfo();
+    student2.displayInfo();
+
+    return 0;
+}
